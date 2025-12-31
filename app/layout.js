41b1d45b0293d3089bb93516438b61b7d4a1cabc
rahulnikamcyber <rahulnikamcyber@gmail.com>
2025-12-31 07:10:36 +0530

    add root layout

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Lead Vendor Platform",
+  description: "Lead Vendor Automation Platform"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
